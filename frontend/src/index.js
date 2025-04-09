6ddb1cafb432317cc294135bfd140231337df60c
finickyspider <finickyspider@gmail.com>
2025-04-08 19:47:56 -0700

    refactor index.js for improved structure and readability

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ const darkTheme = createTheme({
   palette: {
     mode: 'dark',
     background: {
-      default: '#121212',  // a dark background color
+      default: '#121212',
       paper: '#1d1d1d'
     },
   },
